3b3c33f98d38fd7c14b6963953f4dd526ee73e77
Minu Han <mwj1205@naver.com>
2026-05-12 18:12:06 +0900

    feat: PostgreSQL 연결

--- src/blueServer.Api/Program.cs
+++ src/blueServer.Api/Program.cs
@@ -1,5 +1,20 @@
+using blueServer.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+// AddDbContext: 필요할 때 GameDbContext 생성해달라고 등록하는 것.
+builder.Services.AddDbContext<GameDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
 app.MapGet("/", () => "Hello World!");
 
