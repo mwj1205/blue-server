46b1e22d63940d16dc532e348d5dbfe5379d8f6c
Minu Han <mwj1205@naver.com>
2026-05-12 17:29:12 +0900

    script: GameDbContext 주석 추가

---
+++ src/blueServer.Infrastructure/GameDbContext.cs
@@ -0,0 +1,21 @@
+/***
+  * File: GameDbContext.cs
+  * Role: DB 연결
+          테이블 관리
+          쿼리 실행
+          변경 사항 저장
+***/
+
+using blueServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace blueServer.Infrastructure;
+
+public class GameDbContext : DbContext
+{
+    public GameDbContext(DbContextOptions<GameDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Player> Players => Set<Player>();
+}
