a333523fb874d0c83d74bc93b2dfc7f8f3d28a50
Minu Han <mwj1205@naver.com>
2026-05-12 18:54:21 +0900

    fix: Player.cs 파일의 id 자료형 long으로 수정

--- src/blueServer.Domain/Entities/Player.cs
+++ src/blueServer.Domain/Entities/Player.cs
@@ -2,7 +2,7 @@ namespace blueServer.Domain.Entities;
 
 public class Player
 {
-    public int Id { get; set; }
+    public long Id { get; set; }
     public string Nickname { get; set; } = "";
     public int Gold { get; set; }
     public int Gem { get; set; }
