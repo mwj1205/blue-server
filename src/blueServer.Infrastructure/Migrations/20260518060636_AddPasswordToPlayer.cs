efb17816114cc2afa81fc9456909d5e0a34e960d
Minu Han <mwj1205@naver.com>
2026-05-18 15:16:12 +0900

    feat: player 테이블에 password column 추가

---
+++ src/blueServer.Infrastructure/Migrations/20260518060636_AddPasswordToPlayer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blueServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordToPlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Players",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Players");
+        }
+    }
+}
