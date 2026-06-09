299bf1ff55b6206d1f947b19b46045e777b9fc5a
Minu Han <mwj1205@naver.com>
2026-06-09 18:34:22 +0900

    feat: LoginHandler DB 조회 기능 추가
    - 기존에는 임시 플레이어 생성
    - 이제 DB에서 플레이어 닉네임 검색
    - 비밀번호 비교
    - 결과 응답 추가 (Success, fail)

--- src/blueServer.Game/Handlers/LoginHandler.cs
+++ src/blueServer.Game/Handlers/LoginHandler.cs
@@ -1,10 +1,20 @@
+using System.Net.Http.Headers;
 using blueServer.Domain.Entities;
 using blueServer.Game.Packets;
+using blueServer.Game.Repositories;
 
 namespace blueServer.Game.Handlers;
 
 public class LoginHandler : IPacketHandler
 {
+    // PlayerRepository만 의존
+    private readonly PlayerRepository _repository;
+
+    public LoginHandler(PlayerRepository repository)
+    {
+        _repository = repository;
+    }
+
     public Opcode Opcode => Opcode.Login;
 
     public async Task HandleAsync(Session session, PacketReader reader)
@@ -12,13 +22,35 @@ public class LoginHandler : IPacketHandler
         var nickname = reader.ReadString();
         var password = reader.ReadString();
         Console.WriteLine($"[Login] {nickname}");
-        Console.WriteLine($"[Login] {password}");
+        Console.WriteLine($"[password] {password}");
 
-        var player = new Player
+        // Repository를 통해 DB 접근
+        // (Repository 내부에서 DbContext를 메서드별로 생성/관리)
+        var player = await _repository.FindByNicknameAsync(nickname);
+
+        if (player == null)
         {
-            Id = 1,
-            Nickname = nickname
-        };
+            await session.SendAsync(
+                new LoginResultPacket
+                {
+                    Success = false,
+                    Message = "Login failed"
+                }.Serialize()
+            );
+            return;
+        }
+
+        if (player.Password != password)
+        {
+            await session.SendAsync(
+                new LoginResultPacket
+                {
+                    Success = false,
+                    Message = "Login failed"
+                }.Serialize()
+            );
+            return;
+        }
 
         session.Login(player);
 
