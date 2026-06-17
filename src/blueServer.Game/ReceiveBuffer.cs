a8d69b713353462e2984107a80ec6b348c8a96cd
Minu Han <mwj1205@naver.com>
2026-06-17 17:47:12 +0900

    fix: 수신 버퍼 용량 초과 방어 추가

--- src/blueServer.Game/ReceiveBuffer.cs
+++ src/blueServer.Game/ReceiveBuffer.cs
@@ -3,44 +3,61 @@ namespace blueServer.Game;
 public class ReceiveBuffer
 {
     private readonly byte[] _buffer;
-
     private int _writePos;
 
     public ReceiveBuffer(int size)
     {
+        if (size <= 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(size),
+                "Receive buffer size must be positive.");
+        }
+
         _buffer = new byte[size];
-    }
-
-    // 데이터 누적
-    public int Write(byte[] data, int length)
-    {
-        Array.Copy(
-            data,
-            0,
-            _buffer,
-            _writePos,
-            length
-        );
-
-        _writePos += length;
-
-        return _writePos;
     }
 
     public byte[] Buffer => _buffer;
 
     public int Length => _writePos;
 
-    // 처리 완료된 패킷 제거
+    public int Capacity => _buffer.Length;
+
+    public int RemainingCapacity => _buffer.Length - _writePos;
+
+    public int Write(byte[] data, int length)
+    {
+        if (length < 0 || length > data.Length)
+        {
+            throw new ArgumentOutOfRangeException(nameof(length));
+        }
+
+        if (length > RemainingCapacity)
+        {
+            throw new InvalidOperationException(
+                $"Receive buffer capacity exceeded. Capacity={Capacity}, Length={Length}, Incoming={length}.");
+        }
+
+        data.AsSpan(0, length).CopyTo(_buffer.AsSpan(_writePos));
+        _writePos += length;
+
+        return _writePos;
+    }
+
     public void Remove(int length)
     {
-        Array.Copy(
-            _buffer,
-            length,
-            _buffer,
-            0,
-            _writePos - length
-        );
+        if (length < 0 || length > _writePos)
+        {
+            throw new ArgumentOutOfRangeException(nameof(length));
+        }
+
+        if (length == 0)
+        {
+            return;
+        }
+
+        _buffer.AsSpan(length, _writePos - length)
+            .CopyTo(_buffer.AsSpan(0));
 
         _writePos -= length;
     }
